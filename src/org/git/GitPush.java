ca567c61b64d0e9010d61422bbaf815c982d7798
AjithkumarmadurainZ <ajith9319@gmail.com>
2021-12-11 17:33:37 +0530

    Ajithtry

---
+++ src/org/git/GitPush.java
@@ -0,0 +1,14 @@
+package org.git;
+
+
+public class GitPush {
+	public void add() {
+		System.out.println("Addition");
+
+	}
+	public static void main(String[] args) {
+		GitPush g = new GitPush();
+		g.add();
+		}
+
+}
